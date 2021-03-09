5e99da3766909ccc987ff7182a4254e7c488bda3
Mingkai Yu <mingkai2@umbc.edu>
2021-03-09 12:57:28 -0500

    square error

--- four_species_filter.m
+++ four_species_filter.m
@@ -15,10 +15,10 @@ x0 = feval(sys, 'x0');
 Ns = 10000;
 num_trial = 500;
 s1_naive = zeros(num_trial, Ns);
-s2_naive = zeros(num_trial, Ns);
+%s2_naive = zeros(num_trial, Ns);
 w_naive = zeros(num_trial, Ns);
 s1_gt = zeros(num_trial, Ns);
-s2_gt = zeros(num_trial, Ns);
+%s2_gt = zeros(num_trial, Ns);
 w_gt = zeros(num_trial, Ns);
 %%
 tic;
@@ -26,10 +26,10 @@ tic;
 for trial = 1:num_trial
     [V_naive, w_naive(trial,:)] = get_V_w_naive(T, dy, sys, c, Ns);
     s1_naive(trial,:) = V_naive(1,:);
-    s2_naive(trial,:) = V_naive(2,:);
+    %s2_naive(trial,:) = V_naive(2,:);
     [V_gt, w_gt(trial,:)] = get_V_wl_four_species(T, t1, sys, dy, c, Ns);
     s1_gt(trial,:) = V_gt(1,:);
-    s2_gt(trial,:) = V_gt(2,:);
+    %s2_gt(trial,:) = V_gt(2,:);
 end
 toc;
 
@@ -64,7 +64,7 @@ ylabel('Conditional probability')
 hold off
 legend([l1, l6, l11], {'naive', 'two stage', 'ODE'})
 saveas(gcf, 's1-x3_4-x4_7_.png')
-%%
+%% Plot S2
 %{
 figure
 xmin = 0;
@@ -97,13 +97,11 @@ hold off
 saveas(gcf, 's2.png')
 %}
 
-
 %% Plot
 xmin = 0;
 xmax = sum(x0);
 x_range = xmin:xmax;
 
-num_trial = 500;
 x_prob_naive = zeros(num_trial, xmax-xmin+1);
 x_prob_gt = zeros(num_trial, xmax-xmin+1);
 
@@ -134,31 +132,34 @@ saveas(gcf, 'ci-x3_4-x4_7.png')
 
 %% dealing with NaN
 %
+x_prob_naive_clean = x_prob_naive(isfinite(x_prob_naive));
+[num_clean, Ns] = size(x_prob_naive_clean);
 
-%%
-%{
+err_gt = x_prob_gt - pi1';
+err_naive = x_prob_naive_clean - pi1';
+
+bias_gt = mean(err_gt);
+bias_naive = mean(err_naive);
+
 figure
-xmin = 0;
-xmax = sum(x0);
-x_range = xmin:xmax;
-x_prob_naive = zeros(num_trial, xmax-xmin+1);
-x_prob_gt = zeros(num_trial, xmax-xmin+1);
-
-for i = 1:num_trial
-    x_prob_naive(i,:) = get_hist(s2_naive(i,:), w_naive(i,:), x_range);
-    x_prob_gt(i,:) = get_hist(s2_gt(i,:), w_gt(i,:), x_range);
-end
-
-hold on
-errorbar(x_range, mean(x_prob_naive), 2*std(x_prob_naive)/sqrt(num_trial), '-r','LineWidth', 1)
-errorbar(x_range, mean(x_prob_gt), 2*std(x_prob_gt)/sqrt(num_trial), '-b','LineWidth', 1)
-plot(x_range, pi2, '-g*')
-
-
-xlabel('Copy number of S2')
-ylabel('Conditional probability')
-lgd=legend('naive', 'two stage', 'ODE');
-
+errorbar(x_range, bias_gt, 2*std(err_gt)/sqrt(num_trial));
+errorbar(x_range, bias_naive, 2*std(err_naive)/sqrt(num_clean));
+xlabel('Copy number of S1')
+ylabel('Difference with ODE solution')
+lgd=legend('naive', 'two stage');
+%lgd.Location = 'north';
 hold off
-saveas(gcf, 'S2-ci.png')
-%}
+saveas(gcf, 'ci-x3_9-x4_7.png')
+
+sq_err_gt = sqrt(mean(err_gt.^2));
+sq_err_naive = sqrt(mean(err_naive.^2));
+
+figure
+errorbar(x_range, sq_err_gt, 2*std(err_gt.^2)/sqrt(num_trial));
+errorbar(x_range, bias_naive, 2*std(err_naive.^2)/sqrt(num_clean));
+xlabel('Copy number of S1')
+ylabel('Difference with ODE solution')
+lgd=legend('naive', 'two stage');
+%lgd.Location = 'north';
+hold off
+saveas(gcf, 'ci-x3_9-x4_7.png')
